549f52330978ed6c775f95cd3f30f6090c1a3620
tmichell13 <tmichell13@yahoo.com>
2026-09-25 13:43:59 -0400

    initial project set up

---
+++ src/WaveformToolkit.Core/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace WaveformToolkit.Core
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ tests/WaveformToolkit.Core.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace WaveformToolkit.Core.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
